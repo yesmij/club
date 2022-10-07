938741674d9bfbd9b26622cb746e0a926e4142e7
CHO JUN <yesmij@gmail.com>
2022-10-07 22:26:46 +0900

    library setting &
    App Config

--- src/main/java/com/nagesoft/club/ClubAppApplication.java
+++ src/main/java/com/nagesoft/club/ClubAppApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ClubApplication {
+public class ClubAppApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(ClubApplication.class, args);
+        SpringApplication.run(ClubAppApplication.class, args);
     }
 
 }
