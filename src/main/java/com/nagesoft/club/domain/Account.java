010eb0f2c6f6792ee9468afb0e56b3e4d5ce9890
CHO JUN <yesmij@gmail.com>
2022-10-09 18:27:42 +0900

    Account Controller & sign-up form

--- src/main/java/com/nagesoft/club/domain/Account.java
+++ src/main/java/com/nagesoft/club/domain/Account.java
@@ -1,12 +1,14 @@
 package com.nagesoft.club.domain;
 
-import lombok.Getter;
-import lombok.Setter;
+import lombok.*;
+import org.hibernate.annotations.Fetch;
 
 import javax.persistence.*;
+import java.time.LocalDateTime;
 
 @Getter @Setter
-@Entity
+@Entity @EqualsAndHashCode(of = "id")
+@AllArgsConstructor @NoArgsConstructor
 public class Account {
 
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -15,8 +17,27 @@ public class Account {
     @Column(unique = true)
     private String email;
 
+    private String emailCheckToken;
+    private boolean emailChecked;
+
     private String password;
+    @Column(unique = true)
     private String nickname;
 
+    @Lob @Basic(fetch = FetchType.EAGER)
     private String profileImg;
+
+    private String bio;
+    private String url;
+    private String occupation;
+    private String location;
+
+    private LocalDateTime joinedAt;
+
+    private boolean studyCreatedByEmail;
+    private boolean studyCreatedByWeb;
+    private boolean studyEnrollmentResultByEmail;
+    private boolean studyEnrollmentResultByWeb;
+    private boolean studyUpdatedByEmai;
+    private boolean studyUpdatedByWeb;
 }
