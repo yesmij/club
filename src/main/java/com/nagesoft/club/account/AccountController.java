2ae5a1179a1ea1082e1e67821c490dbca0438e27
CHO JUN <yesmij@gmail.com>
2022-10-12 23:46:34 +0900

    SignUpForm Dto & thymeleaf view

--- src/main/java/com/nagesoft/club/account/AccountController.java
+++ src/main/java/com/nagesoft/club/account/AccountController.java
@@ -1,13 +1,15 @@
 package com.nagesoft.club.account;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class AccountController {
 
     @GetMapping("/sign-up")
-    public String singUp() {
+    public String singUp(Model model) {
+        model.addAttribute("signUpForm", new SignUpForm());
         return "account/sign-up";
     }
 }
---
+++ src/main/java/com/nagesoft/club/account/SignUpForm.java
@@ -0,0 +1,12 @@
+package com.nagesoft.club.account;
+
+import lombok.Data;
+
+@Data
+public class SignUpForm {
+
+    private String nickname;
+    private String email;
+    private String password;
+
+}
