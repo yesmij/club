2db25757011b58243f2ba0c1430af2b1ed5b84e5
CHO JUN <yesmij@gmail.com>
2022-10-13 23:33:52 +0900

    javax validation

--- src/main/java/com/nagesoft/club/account/SignUpForm.java
+++ src/main/java/com/nagesoft/club/account/SignUpForm.java
@@ -2,11 +2,21 @@ package com.nagesoft.club.account;
 
 import lombok.Data;
 
+import javax.validation.constraints.*;
+
 @Data
 public class SignUpForm {
 
+    @NotBlank
+    @Min(3) @Max(10)
     private String nickname;
+
+    @NotBlank
+    @Email
     private String email;
+
+    @NotBlank
+    @Min(8)
     private String password;
 
 }
