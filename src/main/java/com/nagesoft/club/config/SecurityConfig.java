188f343217e07499006130e2ebfa58b86ca490dc
CHO JUN <yesmij@gmail.com>
2022-10-23 15:18:43 +0900

    NPM init, install BS and Setting(Security, git ignore)

--- src/main/java/com/nagesoft/club/config/SecurityConfig.java
+++ src/main/java/com/nagesoft/club/config/SecurityConfig.java
@@ -19,7 +19,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     public void configure(HttpSecurity http) throws Exception {
         http.authorizeHttpRequests()
-                .antMatchers("/", "/sign-up", "/login",
+                .mvcMatchers("/", "/sign-up", "/login",
                         "/check-email-token", "/account/checked-email-token",
                         "/h2-console/**").permitAll()
                 .anyRequest().authenticated();
@@ -28,6 +28,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     public void configure(WebSecurity web) throws Exception {
         web.ignoring()
+                .mvcMatchers("/node_modules/**")
                 .requestMatchers(PathRequest.toStaticResources().atCommonLocations());
     }
 }
