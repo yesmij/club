33f66f1bb49c5f63cc566bd4bfcf19b1e062a66b
CHO JUN <yesmij@gmail.com>
2022-11-02 10:47:16 +0900

    profile update success/fail With SpringSecurity @UserDetails

---
+++ src/test/java/com/nagesoft/club/settings/SettingsControllerTest.java
@@ -0,0 +1,84 @@
+package com.nagesoft.club.settings;
+
+import com.nagesoft.club.account.AccountRepository;
+import com.nagesoft.club.account.AccountService;
+import com.nagesoft.club.account.SignUpForm;
+import com.nagesoft.club.domain.Account;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.test.context.support.TestExecutionEvent;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.security.test.context.support.WithUserDetails;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@AutoConfigureMockMvc
+@SpringBootTest
+class SettingsControllerTest {
+
+    @Autowired MockMvc mockMvc;
+    @Autowired AccountService accountService;
+    @Autowired AccountRepository accountRepository;
+
+    @BeforeEach
+    void before() {
+        SignUpForm signUpForm = SignUpForm.builder()
+                .nickname("santiago")
+                .email("yesmij@naver.com")
+                .password("1111").build();
+        accountService.accountCreationProcess(signUpForm);
+    }
+
+    @WithUserDetails(value = "santiago", setupBefore = TestExecutionEvent.TEST_EXECUTION)
+    @DisplayName("업데이트 성공")
+    @Test
+    void profileUpdate_OK() throws Exception {
+
+        Account account = accountRepository.findByNickname("santiago");
+        mockMvc.perform(post("/settings/profile")
+                .param("bio", "M")
+                .param("occupation", "sw")
+                .param("location", "hanam")
+                .param("url", "https://www")
+                .with(csrf()))
+                .andExpect(status().is3xxRedirection())
+                .andExpect(flash().attributeExists("message"));
+
+        Account updateAccount = accountRepository.findByNickname("santiago");
+
+        Assertions.assertThat(account).isEqualTo(updateAccount);
+        Assertions.assertThat("M").isEqualTo(updateAccount.getBio());
+    }
+
+
+    @WithUserDetails(value = "santiago", setupBefore = TestExecutionEvent.TEST_EXECUTION)
+    @DisplayName("업데이트 실패")
+    @Test
+    void profileUpdate_Fail() throws Exception {
+
+        Account account = accountRepository.findByNickname("santiago");
+        mockMvc.perform(post("/settings/profile")
+                        .param("bio", "MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM")
+                        .param("occupation", "sw")
+                        .param("location", "hanam")
+                        .param("url", "https://www")
+                        .with(csrf()))
+                .andExpect(status().isOk())
+                .andExpect(model().hasErrors());
+
+        Account updateAccount = accountRepository.findByNickname("santiago");
+
+//        Assertions.assertThat(account).isNotEqualTo(updateAccount);
+        Assertions.assertThat("null").isNotEqualTo(updateAccount.getBio());
+        assertNull(updateAccount.getOccupation());
+    }
+}
