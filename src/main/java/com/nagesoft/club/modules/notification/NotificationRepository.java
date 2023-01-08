b4ccef8c983a2ef893252ac4effc15eb3328fd2d
CHO JUN <yesmij@gmail.com>
2023-01-08 15:57:16 +0900

    Notification Interceptor / WebConfig, view

--- src/main/java/com/nagesoft/club/modules/notification/NotificationRepository.java
+++ src/main/java/com/nagesoft/club/modules/notification/NotificationRepository.java
@@ -1,6 +1,8 @@
 package com.nagesoft.club.modules.notification;
 
+import com.nagesoft.club.modules.account.Account;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface NotificationRepository extends JpaRepository<Notification, Long> {
+    Long countByAccountAndChecked(Account account, boolean checked);
 }
